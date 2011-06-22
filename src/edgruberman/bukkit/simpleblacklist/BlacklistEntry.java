763cdb3315c4ffc173160b2abd0deb9c24e82b1b
EdGruberman <ed@rjump.com>
2011-06-21 23:13:11 -0700

    CHG: Switch to AccessControl for security and group management.

--- src/edgruberman/bukkit/simpleblacklist/BlacklistEntry.java
+++ src/edgruberman/bukkit/simpleblacklist/BlacklistEntry.java
@@ -4,18 +4,18 @@ import java.util.List;
 
 import org.bukkit.entity.Player;
 
-import edgruberman.bukkit.simplesecurity.AccessControlList;
+import edgruberman.bukkit.accesscontrol.SimpleAccess;
 
 public class BlacklistEntry {
     
     private int id;
     private String message;
-    private AccessControlList acl;
+    private SimpleAccess access;
     
     protected BlacklistEntry(int id, String message, List<String> access) {
         this.id = id;
         this.message = message;
-        this.acl = new AccessControlList(access);
+        this.access = new SimpleAccess(access);
     }
     
     protected int getId() {
@@ -27,6 +27,6 @@ public class BlacklistEntry {
     }
     
     protected boolean isAllowed(Player player) {
-        return this.acl.isAllowed(player);
+        return this.access.isAllowed(player);
     }
 }
